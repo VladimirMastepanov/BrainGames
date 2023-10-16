3e07366e78307c3bc35d7cbcf1a0819bd74d6837
VladimirMastepanov <rozenkranc4@gmail.com>
2023-10-16 14:15:30 +0200

    add greatest common divisor game brain-gcd

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/fb6ca09343f6ffebe1ec/maintainability)](https://codeclimate.com/github/VladimirMastepanov/frontend-project-44/maintainability)
 [![asciicast](https://asciinema.org/a/uPgpSmM8O9OT3TlTavyTefquL.svg)](https://asciinema.org/a/uPgpSmM8O9OT3TlTavyTefquL)
 [![asciicast](https://asciinema.org/a/T0Mkn6u5BtZSxeTWJMnSoADxA.svg)](https://asciinema.org/a/T0Mkn6u5BtZSxeTWJMnSoADxA)
+[![asciicast](https://asciinema.org/a/b48gyTXphHIjWRT2dPadlIdkN.svg)](https://asciinema.org/a/b48gyTXphHIjWRT2dPadlIdkN)
---
+++ src/games/gcd.js
@@ -0,0 +1,22 @@
+import app from '../index.js';
+
+const jobCondition = 'Find the greatest common divisor of given numbers.';
+
+const greatestCommonDivisor = () => {
+  const firstNumber = Math.ceil(Math.random() * 10);
+  const secondNumber = Math.ceil(Math.random() * 10);
+
+  const arithmeticExpression = `${firstNumber} ${secondNumber}`;
+
+  let greatestDivisor;
+  for (let i = 1; i <= firstNumber && i <= secondNumber; i += 1) {
+    if (firstNumber % i === 0 && secondNumber % i === 0) {
+      greatestDivisor = i;
+    }
+  }
+
+  const arr = [arithmeticExpression, greatestDivisor];
+  return arr;
+};
+
+export default () => app(jobCondition, greatestCommonDivisor);
