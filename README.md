0cbc6554c67601fc269aa4d1fc81d1addbd3e55b
VladimirMastepanov <rozenkranc4@gmail.com>
2023-10-16 15:03:34 +0200

    add progression game

--- README.md
+++ README.md
@@ -4,3 +4,4 @@
 [![asciicast](https://asciinema.org/a/uPgpSmM8O9OT3TlTavyTefquL.svg)](https://asciinema.org/a/uPgpSmM8O9OT3TlTavyTefquL)
 [![asciicast](https://asciinema.org/a/T0Mkn6u5BtZSxeTWJMnSoADxA.svg)](https://asciinema.org/a/T0Mkn6u5BtZSxeTWJMnSoADxA)
 [![asciicast](https://asciinema.org/a/b48gyTXphHIjWRT2dPadlIdkN.svg)](https://asciinema.org/a/b48gyTXphHIjWRT2dPadlIdkN)
+[![asciicast](https://asciinema.org/a/ig2rQVg0Ly4jnXhX4OtyMpt1f.svg)](https://asciinema.org/a/ig2rQVg0Ly4jnXhX4OtyMpt1f)
---
+++ src/games/progression.js
@@ -0,0 +1,22 @@
+import app from '../index.js';
+
+const jobCondition = 'What number is missing in the progression?';
+
+const missingNumber = () => {
+  let firstNumber = Math.ceil(Math.random() * 10);
+  const secondNumber = Math.ceil(Math.random() * 5);
+  const arrOfProgression = [firstNumber];
+  const arrOfNumbers = [firstNumber];
+  for (let i = 0; i < 10; i += 1) {
+    firstNumber += secondNumber;
+    arrOfProgression.push(firstNumber);
+    arrOfNumbers.push(firstNumber);
+  }
+  const skippingNumber = Math.ceil(Math.random() * 10);
+  arrOfProgression[skippingNumber] = '..';
+
+  const arr = [arrOfProgression, arrOfNumbers[skippingNumber]];
+  return arr;
+};
+
+export default () => app(jobCondition, missingNumber);
